7cacf895808cb7728b1fb602ec18583ae51b1081
Jerry Zhang <jerryfish@163.com>
2022-03-10 10:27:13 +1300

    1 complete
    feat: 1 complete

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,19 +3,28 @@
 
 import * as React from 'react'
 
-function Greeting() {
+// The tutorial set the default value for initialName to avoid the warning
+// in case of no initialName prop available.
+// But in the practice of my setup, there is no such a warning .
+// I guess the latest React probably do some tweaks here.
+// Anyway, this kind of warning can be solved by Typescript or other means.
+function Greeting({ initialName = '' }) {
   // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+  // const name = ''
+
+  const [name, setName] = React.useState(initialName)
 
   function handleChange(event) {
     // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
+  // Don't forget set the input value as name in extra
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
