a2907f0d5cd93a16282fcb0c345faa60ce3687bf
Jerry Zhang <jerryfish@163.com>
2022-03-11 11:22:51 +1300

    Complet 2-1, 2-2, and 2-3
    feat: complet 2-1, 2-2, and 2-3
    
    - Complet 2-1 and 2-2
    
    - Complete 2-3 and make some modifications different from the tutorial

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -33,7 +33,8 @@ function Greeting({initialName = ''}) {
  */
 
 
-// Extra 1
+/* 
+// Extra 1 & 2
 function Greeting({initialName = ''}) {
   const getInitialFromLocal = () => window.localStorage.getItem('name') ?? initialName
 
@@ -41,7 +42,7 @@ function Greeting({initialName = ''}) {
 
   React.useEffect(() => {
     window.localStorage.setItem('name', name)
-  })
+  }, [name])
 
   function handleChange(event) {
     setName(event.target.value)
@@ -49,6 +50,51 @@ function Greeting({initialName = ''}) {
   return (
     <div>
       <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+ */
+
+
+// I don't think the defaultValue needs to have a '' as the default value
+// if I have set the default value for initialName.
+const useLocalStorageState = (key, defaultValue) => {
+  
+  // Should define the initial function independently,
+  // not as the tutorial directly putting the anonymous function inplementation
+  // as the argument of useState function, which will lead to
+  // printing the function sometimes when refreshing the page
+  // after modifying the code with the input value is empty.
+  const getInitial = () => window.localStorage.getItem(key) ?? defaultValue
+
+  const [state, setState] = React.useState(getInitial)
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  }, [key, state])
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  console.log(initialName)
+  const [name, setName] = useLocalStorageState('name', initialName)
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name: </label>
         <input value={name} onChange={handleChange} id="name" />
       </form>
