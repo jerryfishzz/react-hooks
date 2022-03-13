cb4ec2ef93d7e6dd8d87887f5ec30352ae331ee0
jerryfishzz <jerryfish@163.com>
2022-03-13 14:25:09 +1300

    Use function in useState directly
    feat: use function in useState directly
    
    - The previous problem of printing function out only occurs in Firefox. So just write the function directly into useState.
    
    - Add comments on the modificaiton

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -36,13 +36,16 @@ function Greeting({initialName = ''}) {
 /* 
 // Extra 1 & 2
 function Greeting({initialName = ''}) {
-  const getInitialFromLocal = () => window.localStorage.getItem('name') ?? initialName
+  // const getInitialFromLocal = () => window.localStorage.getItem('name') ?? initialName
 
-  const [name, setName] = React.useState(getInitialFromLocal)
-
+  // const [name, setName] = React.useState(getInitialFromLocal)
+  const [name, setName] = React.useState(
+    () => window.localStorage.getItem('name') ?? initialName,
+  )
+  
   React.useEffect(() => {
     window.localStorage.setItem('name', name)
-  }, [name])
+  })
 
   function handleChange(event) {
     setName(event.target.value)
@@ -60,19 +63,39 @@ function Greeting({initialName = ''}) {
  */
 
 
+
+// Extra 3
 // I don't think the defaultValue needs to have a '' as the default value
 // if I have set the default value for initialName.
-const useLocalStorageState = (key, defaultValue) => {
+// Solved:
+// Since this is a generic function, 
+// it doesn't have to be initialName but anything else.
+// In this case, we give initialName a default value in the Greeting,
+// but in other cases, nobody knows if all the parameters
+// have default values.
+const useLocalStorageState = (key, defaultValue = '') => {
   
   // Should define the initial function independently,
   // not as the tutorial directly putting the anonymous function inplementation
   // as the argument of useState function, which will lead to
-  // printing the function sometimes when refreshing the page
-  // after modifying the code with the input value is empty.
-  const getInitial = () => window.localStorage.getItem(key) ?? defaultValue
+  // printing the function out when refreshing the page
+  // after modifying the code with the input value leaving blank.
+  // Solved: 
+  // The above problem only occurs in Firefox.
+  // Looks like Firefox has some weird behaviors in local storage
+  // that will lead to unexpected results.
+  // It doesn't have to be like the example described above,
+  // for example, the local storage value can be deleted after the code modified,
+  // even only the modification is switching on/off console.log
+  // which should not affect the local storage value.
+  
+  // const getInitial = () => window.localStorage.getItem(key) ?? defaultValue
 
-  const [state, setState] = React.useState(getInitial)
-
+  // const [state, setState] = React.useState(getInitial)
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) ?? defaultValue
+  )
+  
   React.useEffect(() => {
     window.localStorage.setItem(key, state)
   }, [key, state])
@@ -81,7 +104,6 @@ const useLocalStorageState = (key, defaultValue) => {
 }
 
 function Greeting({initialName = ''}) {
-  console.log(initialName)
   const [name, setName] = useLocalStorageState('name', initialName)
 
   function handleChange(event) {
